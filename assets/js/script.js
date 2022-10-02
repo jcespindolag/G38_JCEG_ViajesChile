aca5353092d447bb5865031865584cbe74711a6f
Espindola Garrido Juan Carlos <jespindola@hogardecristo.cl>
2022-10-02 08:03:21 -0300

    Correción margenes secciones 'Quienes somos' y 'Contacto´

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,10 @@ $(function () {
 /*Modifica propiedades de footer cuando pasa del carrusel*/
  $(document).scroll(function () {
     const y = $("html").scrollTop();
+      //alert(y);
       y > 550 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+      y >= 0 & y<= 700 ? $("#nav1").addClass("active") : $("#nav1").removeClass("active");
+      y >= 700 & y<= 900 ? $("#nav2").addClass("active") : $("#nav2").removeClass("active");
+      y >= 900 & y<= 1400 ? $("#nav3").addClass("active") : $("#nav3").removeClass("active");
+      y >= 1400 ? $("#nav4").addClass("active") : $("#nav4").removeClass("active");
   });  
