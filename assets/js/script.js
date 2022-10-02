51174362e071e91b98784d79bcf193ac42fd20e6
Espindola Garrido Juan Carlos <jespindola@hogardecristo.cl>
2022-10-02 18:43:54 -0300

    Agrega validaciones al fomulario de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,6 @@ $(function () {
     $('[data-bs-toggle="tooltip"]').tooltip();
   });
 
-/*Agrega alerta al enviar el mensaje de contacto*/
-$("#enviar").click(function() {
-    alert("El mensaje fue enviado... pronto nos pondremos en contacto con usted. Gracias!!!");
-  });
-
 /*Modifica propiedades de footer cuando pasa del carrusel*/
  $(document).scroll(function () {
     const y = $("html").scrollTop();
@@ -17,4 +12,22 @@ $("#enviar").click(function() {
       y >= 740 & y<= 1280 ? $("#nav2").addClass("active") : $("#nav2").removeClass("active");
       y >= 1280 & y<= 1780 ? $("#nav3").addClass("active") : $("#nav3").removeClass("active");
       y >= 1780 ? $("#nav4").addClass("active") : $("#nav4").removeClass("active");
-  });  
+  });
+  
+  /*Validación completitud de campos en el formulario*/
+  $(function() {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms).forEach( function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }else{
+          alert ("El mensaje fue enviado... pronto nos pondremos en contacto con usted. Gracias!!!")
+          $('#formulario')[0],reset()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
